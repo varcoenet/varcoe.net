1d2332448e0c9d23fa79b8b7ae035e6a6091d53f
varcoenet <67181866+varcoenet@users.noreply.github.com>
2023-10-10 03:04:05 -0400

    upload all

---
+++ dist/pages/index.astro.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('../chunks/pages/index_c3a707d1.mjs');
+
+export { page };
